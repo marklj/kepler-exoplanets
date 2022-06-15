dd1353840fa7dc4ff9791c842ab765a6a3d47ca0
Mark Johnson <marklj@gmail.com>
2022-06-15 09:43:21 -0700

    filter habitable planets

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const { parse } = require('csv-parse')
-const fs = require('fs')
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
 const results = [];
-fs.createReadStream('kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    results.push(data)
+
+// https://www.centauri-dreams.org/2015/01/30/a-review-of-the-best-habitable-planet-candidates/
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
   })
-  .on('error', (e) => {
-    console.error('ERROR', e);
+  .on("error", (e) => {
+    console.error("ERROR", e);
   })
-  .on('end', () => {
+  .on("end", () => {
     console.log(results);
-    console.log('DONE');
-  })
+    console.log(`${results.length} habitable planets found.`);
+  });
